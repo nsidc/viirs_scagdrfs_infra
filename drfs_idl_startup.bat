ae4c758799b2082ddedc724edee4d7d4afcae989
Robyn Marowitz <robyn.marowitz@colorado.edu>
2026-03-16 16:07:57 -0600

    wip

--- drfs_idl_startup.bat
+++
@@ -1,9 +0,0 @@
-; /projects/roma8902/scagdrfs_infra/tasks/drfs_idl_startup.bat
-; IDL routines that can be compiled with -idl_startup cmdline option
-
-.run /projects/roma8902/scagdrfs_infra/scagdrfs_jpl/snowHydro/read_modis_tile.pro
-.run /projects/roma8902/scagdrfs_infra/scagdrfs_jpl/snowHydro/drfs_hdf_solar.pro
-.run /projects/roma8902/scagdrfs_infra/scagdrfs_jpl/snowHydro/extract_modis_reflectance.pro
-.run /projects/roma8902/scagdrfs_infra/scagdrfs_jpl/snowHydro/mod_drfs_v1_2.pro
-.run /projects/roma8902/scagdrfs_infra/scagdrfs_jpl/snowHydro/cleanse_scag.pro
-.run /projects/roma8902/scagdrfs_infra/scagdrfs_jpl/snowHydro/moddrfs_cleanse.pro
