4e4be7a1661d8da00112befc26d008c68b040a1d
Scott Stewart <scotts@colorado.edu>
2026-05-11 11:29:53 -0600

    start generalizing cloudwater masking

--- src/masking.py
+++ src/masking.py
@@ -31,16 +31,35 @@ def h2o16(x, y):
         return y
 
 
-def cw_mask(cloud_mask, water, data):
+def calc_cloud_mask(cloud_data):
+    # Return boolean cloud mask
+    return cloud_data != 0
+
+
+def calc_water_mask(water_data):
+    # Return boolean water mask
+    return water == 100
+
+
+def cw_mask(cloud_data, water_data, data):
     # Apply cloud and water masks
 
-    is_cloud = cloud_mask != 0
-    is_water = water == 100
+    if data.dtype == np.uint8:
+       cloud_flagval = 250
+       water_flagval = 235
+    elif data.dtype == np.uint16:
+       cloud_flagval = 2500
+       water_flagval = 2350
+    else:
+       raise ValueError(f'Cannot determine flag values for data type: {data.dtype}')
+
+    is_cloud = calc_cloud_mask(cloud_data)
+    is_water = calc_water_mask(water_data)
 
     data_cloud_masked = data.copy()
-    data_cloud_masked[is_cloud] = 250
+    data_cloud_masked[is_cloud] = cloud_flagval
 
     data_cloudwater_masked = data_cloud_masked
-    data_cloudwater_masked[is_water] = 235
+    data_cloudwater_masked[is_water] = water_flagval
 
     return data_cloudwater_masked
