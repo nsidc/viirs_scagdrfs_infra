d7b5d390940c940cc89c2ee06051246b56a3395e
Robyn Marowitz <robyn.marowitz@colorado.edu>
2026-05-26 10:41:24 -0600

    remove cloning of scagdrfs_jpl

--- scripts/clone_and_build.sh
+++ scripts/clone_and_build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Get the scag and scagdrfs_jpl repositories
+# Get the scag repository
 source ${PWD}/config/env.sh
 cd ${TOPDIR}
 
 if [ ! -d ${TOPDIR}/scag ]; then
     echo "Cloning scag repository."
     git clone git@github.com:nsidc/scag.git
-fi
-if [ ! -d ${TOPDIR}/scagdrfs_jpl ]; then
-    echo "Cloning scagdrfs_jpl repository."
-    git clone git@github.com:nsidc/scagdrfs_jpl.git
 fi
 
 # Set environment variables and build executables
@@ -19,10 +15,6 @@ echo "Building executables in repositories..."
 echo "Building scag"
 cd ${TOPDIR}/scag
 make build-alpine
-
-#echo "Building scagdrfs"
-#cd ${TOPDIR}/scagdrfs_jpl
-#ln -s pathdef_R2021b.m pathdef.m
 
 cd $TOPDIR
 
