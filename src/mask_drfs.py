e8e869272696df395306df66bb79a6811f7b467e
Scott Stewart <scotts@colorado.edu>
2026-07-07 17:20:41 -0600

    read bip as signed-int16

--- src/mask_drfs.py
+++ src/mask_drfs.py
@@ -31,7 +31,7 @@ def get_data(filename, data_type, error_value):
 def get_cloud_mask_6band(working_dir, src_root, file_info):
     bip_file = Path(working_dir) / (src_root + file_info.get("FILE_INFO", "BIP_SUFFIX"))
     with open(bip_file, "rb") as fbip:
-        data = np.fromfile(fbip, dtype=np.uint16)
+        data = np.fromfile(fbip, dtype=np.int16)
     try:
         data = data.reshape(2400, 2400, 7)
     except ValueError as e:
